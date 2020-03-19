4933af93af55ba13b7af64b89839d271cb8c0293
Sangwoo Joh <lambdasnow@gmail.com>
2020-03-19 11:36:55 +0900

    Update istd

--- infer/istd_note.ml
+++ infer/istd_note.ml
@@ -1,3 +1,5 @@
+open Core
+
 module ARList = struct
   (*
   Invariants:
@@ -22,6 +24,28 @@ module ARList = struct
 end
 
 module MaximumSharing = struct
-  module ForHashtbl (H: Caml.Hashtbl.S) = struct
-  end
+  module ForHashtbl (H : Caml.Hashtbl.S) = struct end
+end
+
+module PhysEqual = struct
+  let rec compare_fields ox oy i =
+    i < 0 || (phys_equal (Obj.field ox i) (Obj.field oy i) && compare_fields ox oy (i - 1))
+
+  let shallow_equal x y =
+    phys_equal x y
+    ||
+    let ox = Obj.repr x in
+    let oy = Obj.repr y in
+    let tx = Obj.tag ox in
+    let ty = Obj.tag oy in
+    Int.equal tx ty && tx < Obj.no_scan_tag
+    &&
+    let sx = Obj.size ox in
+    let sy = Obj.size oy in
+    Int.equal sx sy && compare_fields ox oy (sx - 1)
+
+  let optim1 ~res x = if shallow_equal res x then x else res
+
+  let optim2 ~res x1 x2 =
+    if shallow_equal res x1 then x1 else if shallow_equal res x2 then x2 else res
 end
