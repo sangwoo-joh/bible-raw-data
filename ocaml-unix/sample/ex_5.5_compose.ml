a83e1a3c8e8142c1420263befaedc8d3b17a560d
Sangwoo Joh <lambdasnow@gmail.com>
2020-06-05 15:51:45 +0900

    compose example

--- ocaml-unix/sample/ex_5.5_compose.ml
+++ ocaml-unix/sample/ex_5.5_compose.ml
@@ -7,18 +7,25 @@ let compose () =
     let fd_in, fd_out = pipe () in
     match fork () with
     | 0 ->
-        (* parent *)
-        dup2 fd_out stdout ;
+        (* child *)
+        dup2 fd_out stdout (* connect pipe's input to stdout *) ;
         close fd_out ;
         close fd_in ;
         execv "/bin/sh" [|"/bin/sh"; "-c"; Sys.argv.(i)|]
     | _ ->
-        dup2 fd_in stdin ; close fd_out ; close fd_in
+        (* parent *)
+        dup2 fd_in stdin (* connect pipe's output to stdin *) ;
+        close fd_out ;
+        close fd_in
   done ;
+  (* for the last command, there's no need to create a new pipe *)
   match fork () with
   | 0 ->
+      (* child *)
       execv "/bin/sh" [|"bin/sh"; "-c"; Sys.argv.(n)|]
   | _ ->
+      (* parent *)
+      (* wait for children to terminate *)
       let rec wait_for_children retcode =
         try
           match wait () with
@@ -26,7 +33,7 @@ let compose () =
               wait_for_children (retcode lor n)
           | pid, _ ->
               wait_for_children 127
-        with Unix_error (ECHILD, _, _) -> retcode
+        with Unix_error (ECHILD (* no child to wait for *), _, _) -> retcode
       in
       exit (wait_for_children 0)
 
